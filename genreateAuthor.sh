7211da5a914460ca8115cfc239afb32f5aba6870
Normich ITOUA <nitoua.dev@gmail.com>
2024-05-15 00:53:37 +0100

    x

--- genreateAuthor.sh
+++ genreateAuthor.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-OUTPUT_FILE="AUTHORS"
-
-github_users=()
-
-
 # Use Git log to get a list of email addresses from the commit history
 cat > "AUTHORS" <<- EOF
 	# This file lists all contributors to the repository.
 
-	$(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
+	$(git -C  log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
 EOF
