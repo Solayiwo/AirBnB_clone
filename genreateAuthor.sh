d992e4a5c6499b27df41f02e0a2cecfda74c7144
Normich ITOUA <nitoua.dev@gmail.com>
2024-05-15 00:50:41 +0100

    Update project structure

--- genreateAuthor.sh
+++ genreateAuthor.sh
@@ -4,18 +4,10 @@ OUTPUT_FILE="AUTHORS"
 
 github_users=()
 
-echo "# This file lists all contributors to the repository." > "$OUTPUT_FILE"
-
-echo "" >> "$OUTPUT_FILE"
 
 # Use Git log to get a list of email addresses from the commit history
-git shortlog -se \
-  | perl -spe 's/^\s+\d+\s+//' \
-  | sed -e '/^CommitSyncScript.*$/d' \
-  | grep -oE '<[^>]*>' \
-  | sed 's/[<>]//g' \
-  | sed 's/[+]//g' \
-  | sed 's/^[0-9]*//'\
-  >> "$OUTPUT_FILE"
+cat > "AUTHORS" <<- EOF
+	# This file lists all contributors to the repository.
 
-echo "List of people contributed to this repo has been generated and saved to $OUTPUT_FILE"
+	$(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
+EOF
