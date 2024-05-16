f7c796b67bcfd999417d065d9c94061a6cba9625
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-05-15 19:12:02 -0700

    BaseModel created

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    Base class for all models, defining
+    common attributes and methods.
+    """
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict_repr = self.__dict__.copy()
+        dict_repr['__class__'] = self.__class__.__name__
+        dict_repr['created_at'] = self.created_at.isoformat()
+        dict_repr['updated_at'] = self.updated_at.isoformat()
+        return dict_repr
